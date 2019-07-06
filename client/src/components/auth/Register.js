e4203429b3b85f1940fc1d798f958fc1f3cdd336
Vrishali <vrishalipal@yahoo.com>
2019-07-05 22:36:44 -0400

    Load User & Set Auth Token

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -43,6 +43,7 @@ const Register = ({ setAlert, register }) => {
             name='name'
             value={name}
             onChange={e => onChange(e)}
+            required
           />
         </div>
         <div className='form-group'>
@@ -52,6 +53,7 @@ const Register = ({ setAlert, register }) => {
             name='email'
             value={email}
             onChange={e => onChange(e)}
+            required
           />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
