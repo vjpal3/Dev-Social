cdd9e97bd21949125e6e27340cd7050e550ffbee
Vrishali <vrishalipal@yahoo.com>
2019-07-06 12:54:31 -0400

    Protected Route for Dashboard & Get Current Profile

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,30 @@
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+
+const initialState = {
+  profile: null, //logged in user's profile as well other users' profile when logged in user visits those pages
+  profiles: [], //The profile listing page
+  repos: [],
+  loading: true,
+  error: {}
+};
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
+}
