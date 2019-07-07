43c2a7241f12ab7d9b998218ec0afd9fb5602c55
Vrishali <vrishalipal@yahoo.com>
2019-07-07 07:32:13 -0400

    Delete Experience, Education & Account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const request = require('request');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
@@ -147,8 +148,8 @@ router.get('/user/:user_id', async (req, res) => {
 //@access   Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove user's posts
-
+    // remove user's posts
+    await Post.deleteMany({ user: req.user.id})
     //remove profile
     await Profile.findOneAndRemove({
       user: req.user.id
