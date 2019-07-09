e0dc484288a4b5d10a6feb29acfabf9c608859a0
Vrishali <vrishalipal@yahoo.com>
2019-07-08 22:32:00 -0400

    Add Post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
+import PostForm from './PostForm';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -19,7 +20,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
         <i className='fas fa-user' />
         Welcome to the Community
       </p>
-      {/* PostForm */}
+      <PostForm />
       <div className=''>
         {posts.map(post => (
           <PostItem key={post._id} post={post} />
